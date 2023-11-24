bbefa49d4d35af4dd3e53a4d53aaa3a3e3269dac
alexsencion <alexandersenc99@gmail.com>
2023-11-24 13:02:30 -0400

    Add different types of operators

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -23,5 +23,66 @@ namespace LinqBooks
         {
             return this.bookCollection;
         }
+
+        public IEnumerable<Book> BooksAfter2000() 
+        {
+            //extension method
+            //return bookCollection.Where(p => p.PublishedDate.Year > 2000); 
+
+            //query expression
+            return from book in bookCollection where book.PublishedDate.Year > 2000 select book;
+
+        }
+
+        public IEnumerable<Book> BooksPageTitle()
+        {
+            //extension method
+            //return bookCollection.Where(p => p.PageCount > 250 && p.Title.Contains("Action")); 
+
+            //query expression
+            return from book in bookCollection where book.PageCount > 250 && book.Title.Contains("Action") select book;
+
+        }
+
+        public bool AllBooksCollection()
+        {
+            return this.bookCollection.All(p=> p.Status!= string.Empty);
+        }
+
+        public bool AllBooks2005()
+        {
+            return this.bookCollection.Any(p => p.PublishedDate.Year == 2005);
+        }
+
+        public IEnumerable<Book> PythonBooks()
+        {
+            return bookCollection.Where(p => p.Categories.Contains("Python"));
+        }
+
+        public IEnumerable<Book> JavaBooksByNameAsc()
+        {
+            return bookCollection.Where(p => p.Categories.Contains("Java")).OrderBy(p => p.Title);
+        }
+
+        public IEnumerable<Book> BooksByPageDesc()
+        {
+            return bookCollection.Where(p => p.PageCount > 450).OrderByDescending(p => p.PageCount);
+        }
+
+        public IEnumerable<Book> BooksTakeOperator()
+        {
+            return bookCollection.Where(p => p.Categories.Contains("Java")).OrderByDescending(p => p.PublishedDate).Take(3);
+        }
+
+        public IEnumerable<Book> BooksSkipOperator()
+        {
+            return bookCollection.Where(p => p.PageCount > 400).Skip(2).Take(2);
+        }
+
+        public IEnumerable<Book> SelectBooks()
+        {
+            return bookCollection.Take(3).Select(p=> new Book() {Title = p.Title, PageCount = p.PageCount});
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,8 +2,40 @@
 
 LinqQueries queries = new LinqQueries();
 
-PrintValues(queries.AllCollection());
+//PrintValues(queries.AllCollection());
 
+// All the collection
+//PrintValues(queries.AllCollection());
+
+// Books After 2000
+//PrintValues(queries.BooksAfter2000());
+
+// Books Page Tittle
+//PrintValues(queries.BooksPageTitle());
+
+// Books Status
+//Console.WriteLine(queries.AllBooksCollection());
+
+// Books 2005
+//Console.WriteLine(queries.AllBooks2005());
+
+// Python Books
+//PrintValues(queries.PythonBooks());
+
+// JavaBooksByName
+//PrintValues(queries.JavaBooksByNameAsc());
+
+// BooksByPageDesc
+//PrintValues(queries.BooksByPageDesc());
+
+// BooksTakeOperator
+//PrintValues(queries.BooksTakeOperator());
+
+// BooksSkipOperator
+//PrintValues(queries.BooksSkipOperator());
+
+// SelectBooks
+//PrintValues(queries.SelectBooks());
 
 void PrintValues(IEnumerable<Book> listOfBooks)
 {
